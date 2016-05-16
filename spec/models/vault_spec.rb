f4eed9b2566bf1a75d0b56450305d41aa438bd18
jbandura <jacek.bandura@netguru.pl>
2016-05-16 17:49:01 +0200

    add validations

--- spec/models/vault_spec.rb
+++ spec/models/vault_spec.rb
@@ -3,4 +3,8 @@ RSpec.describe Vault do
   it { is_expected.to have_attribute('name') }
   it { is_expected.to have_attribute('start_state') }
   it { is_expected.to belong_to('user') }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:start_state) }
+  it { should validate_numericality_of(:start_state) }
 end
