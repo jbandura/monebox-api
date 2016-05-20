d9f9165ba0ac9e2be850032098dc023dbfc9154e
jbandura <jacek.bandura@netguru.pl>
2016-05-20 19:56:57 +0200

    move authentication logic to helpers file

--- app/controllers/api/v1/defaults.rb
+++ app/controllers/api/v1/defaults.rb
@@ -17,28 +17,6 @@ module API
           def logger
             Rails.logger
           end
-
-          def current_user
-            return nil if token_data.nil?
-            User.find_by({
-              email: token_data[:email],
-              authentication_token: token_data[:token]
-            })
-          end
-
-          def authenticated?
-            !!current_user
-          end
-
-          def token_data
-            UserAuthenticator.new.data_from_string_token(
-              request.headers['Authorization']
-            )
-          end
-
-          def unauthorized_error
-            error!('401 Unauthorized', 401)
-          end
         end
 
         rescue_from ActiveRecord::RecordNotFound do |e|
